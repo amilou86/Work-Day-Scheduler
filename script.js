cb745cafc2c927c0d890d820965bc586b7aa72a7
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-01-21 11:56:44 +0000

    today's date

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+window.onload = function () {
+    var todayEl = $('#currentDay');
+    var containerEl = $('#containerBox');
 
+    var today = dayjs();
+    todayEl.text(today.format("[Today is ] D MMM YYYY"));
+
+};
