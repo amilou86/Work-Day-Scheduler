f4af988e017124ba59493560e47a54ac76559c41
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-01-21 13:32:26 +0000

    associated colour styles to times

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ window.onload = function () {
     var today = dayjs();
     todayEl.text(today.format("[Today is ] D MMM YYYY"));
 
-containerEl
-    
+    // Select all time-block elements
+    var timeBlocks = document.querySelectorAll('.time-block');
+
+    // first get current time & date
+    var currentTime = new Date();
+    var currentHour = currentTime.getHours();
+
+    // itirate through each time-block div and get the text content of the div & remove excess whitespace 
+    timeBlocks.forEach(timeBlock => {
+        var hourDiv = timeBlock.querySelector('.hour');
+        var timeText = hourDiv.textContent;
+        var time = timeText.trim();
+
+        // determine if each time-block is past, present or future and apply relevant styles form CSS
+        if (time === currentHour + ':00') {
+            timeBlock.classList.add('present');
+        } else if (time < currentHour + ':00') {
+            timeBlock.classList.add('past');
+        } else {
+            timeBlock.classList.add('future');
+        }
+
+
+    });
+
+
+
+
 };
